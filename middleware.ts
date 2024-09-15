35e7c575eab28ee482d94e50903f14af2b5b8d3c
hashan <hemachandra.hashan@gmail.com>
2024-09-15 12:23:47 +0530

    locales added

--- middleware.ts
+++ middleware.ts
@@ -7,12 +7,16 @@ import { match as matchLocale } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
 function getLocale(request: NextRequest): string | undefined {
+  
   const negotiatorHeaders: Record<string, string> = {}
+
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
   // @ts-ignore locales are readonly
   const locales: string[] = i18n.locales
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
+    locales
+  )
 
   const locale = matchLocale(languages, locales, i18n.defaultLocale)
   return locale
